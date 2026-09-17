dfd156a51f66afe9458aec12ef3cb1cef9448988
Chisefue <25110152@student.hcmute.edu.vn>
2026-09-17 08:31:55 +0700

    first attempt

--- Chuong8/canBangTaiPhienTruyCap.cpp
+++ Chuong8/canBangTaiPhienTruyCap.cpp
@@ -2,13 +2,35 @@
 using namespace std;
 
 int main() {
-    int N, tmp;
+    int N, tmp, count = 0, max = -1;
+    bool check = false, checkConstraint = false;
     unordered_map<int, int> map;
     cin >> N;
     for (int i = 0; i < N; i++) {
         cin >> tmp;
-        if (i == 0) (map[i] == tmp);
+        if (i == 0) map[i] = tmp;
         else map[i] = tmp + map[i-1];
+        if (map[i] == 0 && !checkConstraint) checkConstraint = true;
     }
+    for (int i = 0; i < N; i++)
+    {
+        if (!check && map[i] != 0) {
+            count++;
+        } else if (!check && map[i] == 0) {
+            count++;
+            if (max < count) max = count;
+            count = 0;
+            check = true;
+        } if (check && map[i] != 0) {
+            count++;
+        } else if (check && map[i] == 0) {
+            count++;
+            if (max < count) max = count;
+            count = 0;
+        }
+    }
+    if (!checkConstraint) cout << 0;
+    else cout << count;
+    
     return 0;
 }
