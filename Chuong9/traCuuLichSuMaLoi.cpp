6a33b9dd0e7e3bb348729ac7ca2eed01a8c80764
Chisefue <25110152@student.hcmute.edu.vn>
2026-09-17 16:11:57 +0700

    First try

---
+++ Chuong9/traCuuLichSuMaLoi.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, X;
+    cin >> N >> X;
+    vector<int> a(N);
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+        if (a[i] == X) cout << i << ' ';
+    }
+
+    return 0;
+}
