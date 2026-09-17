2329e4cb60e3f201280ee18c5e0c9bb355478c3a
Chisefue <25110152@student.hcmute.edu.vn>
2026-09-17 08:17:05 +0700

    map

--- Chuong8/canBangTaiPhienTruyCap.cpp
+++ Chuong8/canBangTaiPhienTruyCap.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-    
+    int N, tmp;
+    unordered_map<int, int> map;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> tmp;
+        if (i == 0) (map[i] == tmp);
+        else map[i] = tmp + map[i-1];
+    }
     return 0;
 }
